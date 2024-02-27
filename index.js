3a9f6ec3937e0d9e065699be52394ae5a4b057a2
Marie <ma-aldebert@hotmail.fr>
2024-02-27 18:03:25 +0000

    added link to get back to all cities or homepage

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss [<small>]A[</small>]"
           )}</div>
-        </div>`;
+        </div>
+        <a href="index.html">Back to all cities</a>`;
 }
 
 updateTime();
